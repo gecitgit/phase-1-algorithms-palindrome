4d2da11dceacb8b24fa0a1b237ece265284bb4e4
+ <+>
2022-11-01 21:57:05 -0400

    commit tree 95b62bf2a06337e1105e6d0e66ff984d4010b34f

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word.split("").reverse().join("") === word){
+    return true
+  } else {
+    return false
+  }
+
 }
 
 /* 
   Add your pseudocode here
+  take the string -- flip it around -- go for the === check -- ezpz
+
+  .split will turn 'cat' into 'c', 'a', 't'        ///needs emptyy string
+  do we have areverse?
+  .reverse will give me 't', 'a', 'c'              /// no arg
+  .join will turn array into string so >> 'tac'   ///need string?
+  cat === tac
+  false
+
 */
 
 /*
   Add written explanation of your solution here
+  - no builtin method in JS that I know of so we break it down flip it and reverse it 
 */
 
 // You can run `node index.js` to view these console logs
